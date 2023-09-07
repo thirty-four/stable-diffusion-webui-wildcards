326c88eb2a317914c18c4c9373604d021bf6e4b8
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-09-07 21:05:30 +0800

    Update wildcards.py

--- scripts/wildcards.py
+++ scripts/wildcards.py
@@ -86,11 +86,45 @@ class WildcardsScript(scripts.Script):
 
         if original_prompt != p.all_prompts[0]:
             p.extra_generation_params["Wildcard prompt"] = original_prompt
+        if shared.opts.wildcards_negative_prompts:
+            negative_prompt = p.all_negative_prompts[0]
+
+            for npct in range(len(p.all_negative_prompts)):
+                if not pct in dict_rep:
+                    dict_rep[npct] = {}
+
+                prompt = p.all_negative_prompts[npct]
+                prompt = "".join(self.replace_wildcard(chunk, gen, dict_rep[npct]) for chunk in prompt.split("__"))
+                p.all_negative_prompts[npct] = prompt
+
+            if negative_prompt != p.all_negative_prompts[0]:
+                p.extra_generation_params["Wildcard negative prompt"] = negative_prompt
 
         shared.Shared.d_replacements = dict_rep
 
 def on_ui_settings():
-    shared.opts.add_option("wildcards_same_seed", shared.OptionInfo(False, "Use same seed for all images", section=("wildcards", "Wildcards")))
+    shared.opts.add_option(
+        "wildcards_same_seed",
+        shared.OptionInfo(
+            False, "Use same seed for all images", section=("wildcards", "Wildcards")
+        ),
+    )
+    shared.opts.add_option(
+        "wildcards_recursive",
+        shared.OptionInfo(
+            True,
+            "Recursively search for wildcards",
+            section=("wildcards", "Wildcards"),
+        ),
+    )
+    shared.opts.add_option(
+        "wildcards_negative_prompts",
+        shared.OptionInfo(
+            True,
+            "Use wildcards for negative prompts",
+            section=("wildcards", "Wildcards"),
+        ),
+    )
 
 
 script_callbacks.on_ui_settings(on_ui_settings)
